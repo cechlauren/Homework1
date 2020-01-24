2462e8e90b3f8d83ba518dc219fc518fb7e4ce50
cechlauren <59663720+cechlauren@users.noreply.github.com>
2020-01-23 21:35:02 -0800

    update _main_to include .algs functions

--- Homework1sub/__main__.py
+++ Homework1sub/__main__.py
@@ -1,5 +1,7 @@
 # this file is only called when the package is called from the command
 # line
+import numpy as np
+from .algs import quicksort, bubblesort, insertionsort
 from .run import run_stuff
 
 run_stuff()
